c4e80ae3a7c59ae96e8b70fe44f927f69f31e676
Saoud <69368480+Oudy94@users.noreply.github.com>
2021-02-05 13:54:08 +0100

    Update App.js

--- week2/homework/project/hackyourweather/src/App.js
+++ week2/homework/project/hackyourweather/src/App.js
@@ -6,14 +6,14 @@ import CityWeatherDetails from './components/city-weather-details.js'
 
 function App() {
     const [searchCity, setSearchCity] = useState("");
-    const END_POINT = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&units=metric`;
-    const {data, isLoading, hasError} = useFetchData(END_POINT);
+    const WEATHER_END_POINT = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&units=metric`;
+    const {data, isLoading, hasError} = useFetchData(WEATHER_END_POINT);
 
     return (
         <div className="container">
-            <h1>{"Weather"}</h1>
+            <h1>Weather</h1>
             <SearchBox setSearchCity={setSearchCity} />
-            {<CityWeatherDetails data={data} isLoading={isLoading} hasError={hasError} />}
+            <CityWeatherDetails data={data} isLoading={isLoading} hasError={hasError} />
         </div>
     );
 }
